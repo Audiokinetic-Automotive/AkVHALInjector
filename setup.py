5b536b09645f2fd376a87bec51ffa86157f1dcbc
Vitor Albuquerque <vqalbuquerque@audiokinetic.com>
2024-08-14 15:47:14 -0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
-
 setup(
-    name='AkVHALInjector',
-    version='0.0.1',
-    description='A Python interface to inject VHAL properties using adb.',
+    name="AkVHALInjector",
+    url="https://github.com/Audiokinetic-Automotive/AkVHALInjector",
+    version="0.0.1",
     packages=find_packages(),
+    description="A Python interface to inject VHAL properties using adb.",
+    license="Apache License 2.0",
 )
